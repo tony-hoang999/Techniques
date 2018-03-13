54e70a2ce6ec5ee78dab3550c47077e1d84d2fb8
Tony Hoang <hoangvinhtuong@gmail.com>
2018-03-13 15:50:00 -0500

    Init src

---
+++ Techniques/Techniques/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Techniques
+//
+//  Created by Tuong Hoang on 3/13/18.
+//  Copyright © 2018 tonyhoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
